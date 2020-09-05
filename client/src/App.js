91d76493d206b05a4b31c5f2324c8db2d3c0f7de
NameerWaqas <nameerwaqas321@gmail.com>
2020-09-05 18:57:13 +0500

    Feat: Client side functionality Make client side functional of requesting data through Appolo-Client.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {ApolloClient,InMemoryCache,ApolloProvider,gql,useQuery} from '@apollo/client'
+
+
+const client =new ApolloClient({
+  uri:"http://localhost:5000/graphql",
+  cache:new InMemoryCache()
+})
+
+const users = gql `
+query getUsers {
+  users{
+    name,
+    id
+  }
+}
+`
+
 
 function App() {
+
+
   return (
+    <ApolloProvider client={client}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Hello world of GQL</h1>
+    <GQL/>
     </div>
+    </ApolloProvider>
   );
 }
+
+function GQL(props){
+  const {loading,error,data} = useQuery(users)
+  if(!loading){
+    console.log(data.users)
+  }
+  return(
+    <div>
+      {
+        loading?<h1>Loading..</h1>:data.users.map(user=><div key={user.id}><h3>{user.id} {user.name}</h3></div>)
+      }
+    </div>
+  )
+}
+
 
 export default App;
