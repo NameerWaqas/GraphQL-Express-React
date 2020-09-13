0efd9ad3b87031e72dc33e07f019ee1077d077ab
NameerWaqas <nameerwaqas321@gmail.com>
2020-09-13 15:25:24 +0500

    Refact: Added handlers for user adding form.

--- client/src/components/APIData.js
+++ client/src/components/APIData.js
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { gql, useQuery } from '@apollo/client'
-import { Descriptions, Input } from 'antd';
+import { Descriptions, Input, Button } from 'antd';
 import { context } from './ContextAPI';
 
 
@@ -49,25 +49,111 @@ function GenerateQuery() {
 
 
 function DescriptionsForm(props) {
+  let [change, setChange] = useState({
+    "id": "",
+    "name": "",
+    "username": "",
+    "email": "",
+    "phone": "",
+    "website": "",
+    "city": "",
+    "suite": "",
+    "street": "",
+    "zipcode": "",
+    "cname": "",
+    "catchPhrase": "",
+    "bs": ""
+  })
+  let handleChange = (e) => {
+    let eName = e.target.name
+    let eVal = e.target.value
+    setChange((prevState) => {
+      switch (eName) {
+        case 'id':
+          return { ...prevState, id: eVal };
+        case 'name':
+          return { ...prevState, name: eVal };
+        case 'username':
+          return { ...prevState, username: eVal };
+        case 'email':
+          return { ...prevState, email: eVal };
+        case 'phone':
+          return { ...prevState, phone: eVal }
+        case 'website':
+          return { ...prevState, website: eVal }
+        case 'city':
+          return { ...prevState, city: eVal }
+        case 'suite':
+          return { ...prevState, suite: eVal }
+        case 'zipcode':
+          return { ...prevState, zipcode: eVal }
+        case 'street':
+          return { ...prevState, street: eVal }
+        case 'cname':
+          return { ...prevState, cname: eVal }
+        case 'catchPhrase':
+          return { ...prevState, catchPhrase: eVal }
+        case 'bs':
+          return { ...prevState, bs: eVal }
+        default:
+          return { ...prevState }
+      }
+    })
+    // console.log(e.target.value, e.target.name)
+  }
+
+  let handleAddUser = () => {
+    // let addUserQuery = gql `
+    // mutation {
+    //   addUser(id:${change.id},name:${change.name},username:${change.username},email:${change.email},
+    //           phone:${change.phone},website:${change.website},city:${change.city},suite:${change.suite}
+    //           ,street:${change.street},zipcode:${change.zipcode},cname:${change.cname},bs:${change.bs}
+    //           ,catchPhrase:${change.catchPhrase},lat:"00",lng:"00")
+    //           {
+    //     id
+    //   }
+    // }     
+    // `
+    // let testAdd = gql `
+    // mutation adduser(){
+    //   addUser(id:"12",name:"n",username:"b",email:"c",phone:"d",website:"e",city:"f",street:"g",suite:"h",zipcode:"i"
+    //   ,cname:"A",bs:"B",catchPhrase:"C",lat:"90",lng:"92"){id}
+    // }
+    // `
+    // console.log(testAdd)
+  }
+
   return (
     <Descriptions bordered column={{ lg: 2, md: 2, sm: 2, xs: 1 }} style={{ margin: "1%" }}>
-      <Descriptions.Item label="ID"><Input placeholder="ID" /></Descriptions.Item>
-      <Descriptions.Item label="Name"><Input placeholder="Name" /></Descriptions.Item>
-      <Descriptions.Item label="Username"><Input placeholder="Username" /></Descriptions.Item>
-      <Descriptions.Item label="Email"><Input placeholder="Email" /></Descriptions.Item>
-      <Descriptions.Item label="Phone"><Input placeholder="Phone" /></Descriptions.Item>
-      <Descriptions.Item label="Website"><Input placeholder="Website" /></Descriptions.Item>
+      <Descriptions.Item label="ID">
+        <Input placeholder="ID" name="id" onChange={(e) => handleChange(e)} />
+      </Descriptions.Item>
+      <Descriptions.Item label="Name">
+        <Input placeholder="Name" name="name" onChange={(e) => handleChange(e)} />
+      </Descriptions.Item>
+      <Descriptions.Item label="Username">
+        <Input placeholder="Username" name="username" onChange={(e) => handleChange(e)} />
+      </Descriptions.Item>
+      <Descriptions.Item label="Email">
+        <Input placeholder="Email" name="email" onChange={(e) => handleChange(e)} />
+      </Descriptions.Item>
+      <Descriptions.Item label="Phone">
+        <Input placeholder="Phone" name="phone" onChange={(e) => handleChange(e)} />
+      </Descriptions.Item>
+      <Descriptions.Item label="Website">
+        <Input placeholder="Website" name="website" onChange={(e) => handleChange(e)} />
+      </Descriptions.Item>
       <Descriptions.Item label="Company">
-        <Input placeholder="Company name" />
-        <Input placeholder="Catch phrase" />
-        <Input placeholder="BS" />
+        <Input placeholder="Company name" name="cname" onChange={(e) => handleChange(e)} />
+        <Input placeholder="Catch phrase" name="catchPhrase" onChange={(e) => handleChange(e)} />
+        <Input placeholder="BS" name="bs" onChange={(e) => handleChange(e)} />
       </Descriptions.Item>
       <Descriptions.Item label="address">
-        <Input placeholder="Street" />
-        <Input placeholder="Suite" />
-        <Input placeholder="City" />
-        <Input placeholder="Zip code" />
-
+        <Input placeholder="Street" name="street" onChange={(e) => handleChange(e)} />
+        <Input placeholder="Suite" name="suite" onChange={(e) => handleChange(e)} />
+        <Input placeholder="City" name="city" onChange={(e) => handleChange(e)} />
+        <Input placeholder="Zip code" name="zipcode" onChange={(e) => handleChange(e)} />
+        <Button style={{ marginTop: "1%" }} onClick={handleAddUser}>Add User</Button>
       </Descriptions.Item>
     </Descriptions>
   )
@@ -83,7 +169,7 @@ export default function APIData(props) {
   else {
     return (
       <div>
-        <DescriptionsForm/>
+        <DescriptionsForm />
         {
           data.users.map(user => {
             return <Descriptions key={user.id} bordered column={{ lg: 3, md: 3, sm: 2, xs: 1 }} style={{ margin: "1%" }}>
